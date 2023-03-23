6cd62e8407577ed07601bd0562f005494cb422ab
KacperWawrylo <k.wawrylo@student.uksw.edu.pl>
2023-03-23 20:24:55 +0100

    Lab5B1_dokonczone

--- LAB5B1/Lab5B1.cpp
+++ LAB5B1/Lab5B1.cpp
@@ -1,114 +1,103 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
-#include <windows.h>
 #include <string.h>
 #include <ctype.h>
-#include <math.h>
 
 struct student {
-    int nr_ID;
-    char nazwisko[40];
-    char imie[40];
-    double punkty;
+	int nr_ID;
+	char nazwisko[40];
+	char imie[40];
+	double punkty;
 };
 
-int policz_wiersze(FILE* plik) {
-    int licznik = 0;
-    char c;
-    while ((c = getc(plik)) != EOF) {
-        if (c == '\n') {
-            licznik++;
-        }
-    }
-    rewind(plik);
-    return licznik;
+int policz_wiersze(FILE* f) {
+	int licznik = 0;
+	char c;
+	while ((c = fgetc(f)) != EOF) {
+		if (c == '\n') {
+			licznik++;
+		}
+	}
+	rewind(f);
+	return licznik;
 }
 
 struct student_array {
-    struct student* tablica;
-    int rozmiar;
+	struct student* tablica;
+	int rozmiar;
 };
 
-void wypisz_tablice(struct student* tablica, int rozmiar);
-struct student_array usun_studenta(struct student* tablica, int rozmiar);
+void wypisz_tab(struct student* tablica, int rozmiar);
 
-struct student_array wczytaj_z_pliku(char* nazwa_pliku) {
-    FILE* plik = fopen(nazwa_pliku, "r");
-    if (plik == NULL) {
-        printf("Blad otwarcia pliku.\n");
-        exit(0);
-    }
-    int liczba_wierszy = policz_wiersze(plik);
-    if (liczba_wierszy == 0) {
-        printf("Plik jest pusty.\n");
-        exit(0);
-    }
-    struct student* tablica = (struct student*)malloc(sizeof(struct student) * liczba_wierszy);
-    if (tablica == NULL) {
-        printf("Blad alokacji pamieci.\n");
-        exit(0);
-    }
-    int i;
-    for (i = 0; i < liczba_wierszy; i++) {
-        fscanf(plik, "%d %s %s %lf", &tablica[i].nr_ID, tablica[i].nazwisko, tablica[i].imie, &tablica[i].punkty);
-    }
-    fclose(plik);
-    struct student_array wynik;
-    wynik.tablica = tablica;
-    wynik.rozmiar = liczba_wierszy;
-    wypisz_tablice(tablica, wynik.rozmiar);
-    usun_studenta(tablica, wynik.rozmiar);
-    return wynik;
+struct student_array wczytywanie_do_tab(char* plik) {
+	FILE* f = fopen(plik, "r");
+	if (f == NULL) {
+		exit(0);
+	}
+	int liczba_wierszy = policz_wiersze(f);
+	struct student* tablica = (struct student*)malloc(liczba_wierszy * (sizeof(struct student)));
+	for (int i = 0; i < liczba_wierszy; i++) {
+		fscanf(f, "%d %s %s %lf", &tablica[i].nr_ID, tablica[i].nazwisko, tablica[i].imie, &tablica[i].punkty);
+	}
+	struct student_array wynik;
+	wynik.tablica = tablica;
+	wynik.rozmiar = liczba_wierszy;
+	fclose(f);
+	return wynik;
 }
 
-void wypisz_tablice(struct student* tablica, int rozmiar) {
-    int i;
-    for (i = 0; i < rozmiar; i++) {
-        printf("%d %s %s %.2lf\n", tablica[i].nr_ID, tablica[i].nazwisko, tablica[i].imie, tablica[i].punkty);
-    }
+void wypisz_tab(struct student* tablica, int rozmiar) {
+	for (int i = 0; i < rozmiar; i++) {
+		printf("%d %s %s %.2lf\n", tablica[i].nr_ID, tablica[i].nazwisko, tablica[i].imie, tablica[i].punkty);
+	}
 }
 
-struct student_array usun_studenta(struct student* tablica, int rozmiar) {
-    int n = rozmiar - 1; 
-    printf("podaj id studenta do wyrzucenia: ");
-    int id;
-    scanf_s("%d", &id);
-    int indeks_do_usuniecia = -1;
-    struct student* nowa_tablica = (struct student*)malloc(sizeof(struct student) * (rozmiar - 1));
-    for (int i = 0, j = 0; i < rozmiar; i++) {
-        if (tablica[i].nr_ID == id) {
-            indeks_do_usuniecia = i;
-            break;
-        }
-        else {
-            nowa_tablica[j++] = tablica[i];
-        }
-    }
-    free(tablica);
-    struct student_array wynik;
-    wynik.tablica = nowa_tablica;
-    wynik.rozmiar = rozmiar-1;
-    wypisz_tablice(nowa_tablica, wynik.rozmiar);
-    return wynik;
+struct student_array usuwanie_i_nowa_tab(struct student* tablica, int liczba_wierszy) {
+	liczba_wierszy = liczba_wierszy - 1;
+	struct student* tablica2 = (struct student*)malloc(liczba_wierszy * (sizeof(struct student)));
+	printf("\nPodaj id do usuniecia: ");
+	int id;
+	scanf_s("%d", &id);
+	int indeks_do_usu = -1;
+	int index_2 = 0;
+	for (int i = 0; i < liczba_wierszy+1; i++) {
+		if (tablica[i].nr_ID == id) {
+			indeks_do_usu = i;
+			continue;
+		}
+		tablica2[index_2] = tablica[i];
+		index_2++;
+	}
+	if (indeks_do_usu == -1) {
+		printf("zly indeks");
+		exit(0);
+	}
+	free(tablica);
+	struct student_array wynik2;
+	wynik2.tablica = tablica2;
+	wynik2.rozmiar = liczba_wierszy;
+	return wynik2;
+
 }
 
 int main() {
-    char tab[] = "plik.txt";
-    struct student_array wynik = wczytaj_z_pliku(tab);
+	char tab[] = "plik.txt";
+	struct student_array pierwsza = wczytywanie_do_tab(tab);
+	wypisz_tab(pierwsza.tablica, pierwsza.rozmiar);
+	struct student_array druga = usuwanie_i_nowa_tab(pierwsza.tablica, pierwsza.rozmiar);
+	wypisz_tab(druga.tablica, druga.rozmiar);
 
-    printf("podaj nazwe pliku w ktorym chcesz zapisac: ");
-    char taba[40]{NULL};
-    scanf_s("%s", &tab, 40);
-    
-    FILE* plik = fopen(taba, "w");
-    int rozmiar = wynik.rozmiar - 1;
-    struct student* tablica = wynik.tablica;
-    for (int i = 0; i < rozmiar; i++) {
-        fprintf(plik,"%d %s %s %.2lf\n", tablica[i].nr_ID, tablica[i].nazwisko, tablica[i].imie, tablica[i].punkty);
-    }
+	char zapis[40] = { NULL };
+	printf("podaj plik: ");
+	scanf_s("%s", &zapis,40);
+	FILE* fp = fopen(zapis, "w");
+	for (int i = 0; i < druga.rozmiar; i++) {
+		fprintf(fp, "%d %s %s %.2lf\n", druga.tablica[i].nr_ID, druga.tablica[i].nazwisko, druga.tablica[i].imie, druga.tablica[i].punkty);
+	}
 
-    fclose(plik);
+	fclose(fp);
 
+	system("pause");
 	return 0;
 }
